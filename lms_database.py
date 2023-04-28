7a7cf1cc990c863f2e7fb38d8abbca1b77419075
Dhrutik <solankidhrutik0602@gmail.com>
2023-04-28 00:37:37 -0500

    removed navbar

--- lms_database.py
+++ lms_database.py
@@ -1,14 +1,10 @@
 from tkinter import *
 import set_theme
-import navbar
 import sqlite3
 
 def main():
     window = set_theme.setTheme()
-
-    window_w_navbar = navbar.navBar(window)
-
-    window_w_navbar.mainloop()
+    window.mainloop()
 
 
 
