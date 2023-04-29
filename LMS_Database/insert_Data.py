ac0a704e346dd5df2ce59b6f65ff325de4725992
Dhrutik-S <solankidhrutik0602@gmail.com>
2023-04-29 15:06:07 -0500

    jank queries done

--- LMS_Database/insert_Data.py
+++ LMS_Database/insert_Data.py
@@ -6,7 +6,7 @@ curr_path = os.getcwd()
 curr_data_path = os.path.join(curr_path,'CSVData')
 
 #change the csv file name depending on which file you want to enter
-with open(os.path.join(curr_data_path,'Book_Authors.csv'),'r') as f:
+with open(os.path.join(curr_data_path,'Publisher.csv'),'r') as f:
     readFile = csv.reader(f)
 
     connDB = sqlite3.connect(os.path.join(curr_path,'Database.db'))
@@ -14,7 +14,7 @@ with open(os.path.join(curr_data_path,'Book_Authors.csv'),'r') as f:
 
     for row in readFile:
         #change the table name, fields, VALUES, and rows depending on the table you want to enter
-        connectDB.execute('INSERT INTO BOOK_AUTHORS (book_id, author_name) VALUES (?, ?)',(row[0],row[1]))
+        connectDB.execute('INSERT INTO PUBLISHER (publisher_name,phone,address) VALUES (?,?,?)',(row[0],row[1],row[2]))
 
 connDB.commit()
 connDB.close()
